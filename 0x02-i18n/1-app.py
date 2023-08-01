8e2f226330f769c67723a2da144d3c90e1a9f099
njogued <njogued@gmail.com>
2023-08-02 00:15:24 +0300

    Creating a babel config

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Basic Flask app to render a page"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app,
+              locale_selector=Config.BABEL_DEFAULT_LOCALE,
+              timezone_selector=Config.BABEL_DEFAULT_TIMEZONE)
+
+
+@app.route('/')
+def index_fn():
+    """Method to render the first template"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
