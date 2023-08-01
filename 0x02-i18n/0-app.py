66717c1c0ac6b5c6f60c7ce949806776f380c2cd
njogued <njogued@gmail.com>
2023-08-01 23:34:21 +0300

    Fixed run

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index_fn():
     """Method to render the first template"""
     return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
