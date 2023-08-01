d7ce1e53bf934b342c8a7c60cf04d13fda4b13a0
njogued <njogued@gmail.com>
2023-08-02 00:49:48 +0300

    i18n - Translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Basic Flask app to render a page"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Function to set the local language"""
+    return request.accept_languages.best_match(["en", "fr"])
+
+
+@app.route('/', strict_slashes=False)
+def index_fn():
+    """Method to render the first template"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
