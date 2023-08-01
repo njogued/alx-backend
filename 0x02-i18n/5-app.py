f173996e46f632b6f0777d94bde76b34fa1c983a
njogued <njogued@gmail.com>
2023-08-02 01:47:43 +0300

    Logging in?

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""Basic Flask app to render a page"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Configuration class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Function to set the local language"""
+
+    if 'locale' in request.args:
+        user_lang = request.args['locale']
+        if user_lang in Config.LANGUAGES:
+            return user_lang
+    return request.accept_languages.best_match(["en", "fr"])
+
+
+def get_user(user_id=None):
+    """A function that returns the user details if loginas"""
+    if user_id and user_id in users:
+        return users[user_id]
+    return None
+
+
+@app.before_request
+def before_request():
+    """Find the user details given the login deets"""
+    if 'login_as' in request.args:
+        user_id = int(request.args.get('login_as'))
+        g.user = get_user(user_id)
+    else:
+        g.user = None
+
+
+@app.route('/', strict_slashes=False)
+def index_fn():
+    """Method to render the first template"""
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
