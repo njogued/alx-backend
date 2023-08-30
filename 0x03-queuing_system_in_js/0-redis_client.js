8db9f67aafccdaf058dd8fa6cb3e07d93ad504b0
njogued <njogued@gmail.com>
2023-08-30 10:38:43 +0300

    Connect to a redis client through redis.createClient object

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,6 +1,19 @@
-import { createClient } from 'redis';
+#!/usr/bin/yarn dev
 
+// Import the createClient function from the 'redis' library
+import { createClient } from "redis";
+
+// Create a Redis client instance
 const client = createClient();
-client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
-await client.connect();
-console.log('Redis client connected to the server')
+
+// Event handler for the 'error' event emitted by the Redis client
+client.on("error", (err) => {
+  // Log an error message when the client encounters an error
+  console.log("Redis client not connected to the server:", err.toString());
+});
+
+// Event handler for the 'connect' event emitted by the Redis client
+client.on("connect", () => {
+  // Log a message when the client successfully connects to the server
+  console.log("Redis client connected to the server");
+});
