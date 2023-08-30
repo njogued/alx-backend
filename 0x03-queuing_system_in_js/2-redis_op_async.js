252d4b9ed6c38c9b90b4e6b8d7ab4ebd8e676043
njogued <njogued@gmail.com>
2023-08-30 22:05:13 +0300

    callback to async/await using promisify

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -8,10 +8,6 @@ client.on("error", (err) => {
   console.log("Redis client not connected to the server:", err.toString());
 });
 
-client.on("connect", () => {
-  console.log("Redis client connected to the server");
-});
-
 function setNewSchool(schoolName, value) {
   client.SET(schoolName, value, print);
 }
@@ -23,3 +19,8 @@ async function main() {
   setNewSchool("HolbertonSanFrancisco", "100");
   await displaySchoolValue("HolbertonSanFrancisco");
 }
+
+client.on("connect", async () => {
+  console.log("Redis client connected to the server");
+  await main();
+});
